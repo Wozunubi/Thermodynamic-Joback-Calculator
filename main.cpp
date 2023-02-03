3d54fb121d7c6832cfc0dcbded90ce94ec5a10ea
George F <78384472+Wozunubi@users.noreply.github.com>
2023-02-02 20:26:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,28 @@ const double T = 298.15;
 const double enthFormH2O = -285.83;
 const double enthFormCO2 = -393.51;
 
+double molarM(const std::unordered_map<std::string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
+    double M = 0;
+    for (const auto &x : input) {
+        M += x.second*data[x.first]["M"];
+    }
+    return M;
+}
+
 double normalBP(const std::unordered_map<std::string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
     double Tb = 0;
     for (const auto &x : input) {
         Tb += x.second*data[x.first]["Tb"];
     }
     return Tb+198;
+}
+
+double freezingP(const std:: unordered_map<std:: string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
+    double Tm = 0;
+    for (const auto &x : input) {
+        Tm += x.second*data[x.first]["Tm"];
+    }
+    return Tm+122.5;
 }
 
 double enthForm(const std::unordered_map<std::string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
@@ -41,6 +57,18 @@ double critPres(const std::unordered_map<std::string, int> &input, std::unordere
         N += x.second*data[x.first]["N"];
     }
     return pow(0.113+0.0032*N-Pc, -2);
+}
+
+double critVol(const std::unordered_map<std::string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
+    double Vc = 0;
+    for (const auto &x : input) {
+        Vc += x.second*data[x.first]["Vc"];
+    }
+    return Vc+17.5;
+}
+
+double critComp(const std::unordered_map<std::string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
+    return (critPres(input, data)*critVol(input, data))/(83.1447*critTemp(input, data));
 }
 
 double enthVapTnbp(const std::unordered_map<std::string, int> &input, std::unordered_map<std::string, std::unordered_map<std::string, double>> data) {
@@ -105,6 +133,14 @@ int main() {
         std::cout << "Cannot open data.csv" << "\n";
     }
 
-    std::cout << enthCombAlkanol(input, data) << " kJ mol^-1\n";
+    std::cout << "Molar mass = " << molarM(input, data) << " g mol^-1\n";
+    std::cout << "Normal boiling point = " << normalBP(input, data) << " K\n";
+    std::cout << "Critical temperature = " << critTemp(input, data) << " K\n";
+    std::cout << "Critical pressure = " << critPres(input, data) << " bar\n";
+    std::cout << "Critical volume = " << critVol(input, data) << " cm^3 mol^-1\n";
+    std::cout << "Critical compressibility = " << critComp(input, data) << "\n";
+    std::cout << "Freezing point = " << freezingP(input, data) << " K\n";
+    std::cout << "Enthalpy of formation = " << enthForm(input, data) << " kJ mol^-1\n";
+    //std::cout << "Enthalpy of combustion, alkanol = " << enthCombAlkanol(input, data) << " kJ mol^-1\n";
     return 0;
 }
